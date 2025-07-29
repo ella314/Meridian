1f94a712e57f60fbdf250dd6da7717fdd80540e6
Ella Ragoler <ella.ragoler@gmail.com>
2025-07-29 09:53:40 -0700

    Initial Commit

---
+++ Meridian/MeridianApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeridianApp.swift
+//  Meridian
+//
+//  Created by Scholar on 7/29/25.
+//
+
+import SwiftUI
+
+@main
+struct MeridianApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
